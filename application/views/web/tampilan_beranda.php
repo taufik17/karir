90f834d82b3af3d17ff6444db92412f8d3a9b724
Taufik Agung Santoso <taufikagungsantoso17@gmail.com>
2020-04-19 00:21:54 +0700

    fixed list job beranda
    memperbaiki list job ketika suatu perusahaan pada listnya telah kadaluarsa, maka tidak ditampilkan.

--- application/views/web/tampilan_beranda.php
+++ application/views/web/tampilan_beranda.php
@@ -220,6 +220,11 @@
 																								WHERE perusahaan = $row->Id_perusahaan
         																				AND status = '<span class=\"label label-success\">Telah tayang</span>'
         																				AND deadline >= current_date();");
+
+						$query_buka = $this->db->query("SELECT perusahaan FROM joblist
+																				WHERE status = '<span class=\"label label-success\">Telah tayang</span>'
+																				AND deadline >= current_date();");
+																				
 						$query_tutup = $this->db->query("SELECT * FROM joblist
 																								WHERE perusahaan = $row->Id_perusahaan
 																		        		AND status = '<span class=\"label label-success\">Telah tayang</span>'
@@ -237,128 +242,128 @@
 							?>
 
 							<?php
-							if ($hitung_buka > 1) { ?>
+							if ($hitung_buka >= 1) { ?>
+								<div class="card">
+									<div class="card-header" id="heading-<?= $row->Id_perusahaan ?>">
+										<div class="single-post row">
+											<div class="col-lg-10 col-md-9 profile d-flex align-items-start p-2">
+												<a class="comp-logo col-md-2 pl-0 pr-3" href="<?= base_url(); ?>company/view/<?= $row->Id_perusahaan ?>">
+													<img class="box-shadow p-1" src="<?= base_url() ?>assets/gambar/company/profil/<?= $row->Logo_perusahaan; ?>" width="95px" alt="<?= $row->Nama_perusahaan; ?>">
+												</a>
+												<div>
+													<h4 class="m-0"><a href="<?= base_url(); ?>company/view/<?= $row->Id_perusahaan ?>" class="txt-dark-blue"><strong><?= $row->Nama_perusahaan; ?></strong></a></h4>
+													<span class="d-block txt-dark-blue">
+														<?php foreach ($query2->result() as $prov) ?>
+														<p><?= $prov->nama ?>, Indonesia </p>
+													</span>
+													<p class="mb-1 mt-1"><?= $row->deskripsi_perusahaan; ?></p>
+												</div>
+											</div>
+											<div class="col-lg-2 col-md-3 position text-center d-flex align-items-center p-2">
+												<div class="w-100">
+													<?php
+													$count = 0;
+													foreach ($query->result() as $jobbuka)
+													$count++
+													?>
+												<h1><?= $count ?></h1>
+													<span>Lowongan</span>
+													<h5 class="mb-0">
+														<a data-toggle="collapse" class="<?= $kelas; ?>" href="#collapse-<?= $row->Id_perusahaan ?>" aria-expanded="<?= $ekspand; ?>" aria-controls="collapse-<?= $row->Id_perusahaan ?>">
+														</a>
+													</h5>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div id="collapse-<?= $row->Id_perusahaan ?>" class="<?= $col; ?>" data-parent="#accordion" aria-labelledby="heading-<?= $row->Id_perusahaan ?>">
+										<div class="card-body">
+											<ul class="nav nav-tabs nav-justified" role="tablist">
+												<li class="nav-item">
+													<a class="nav-link active rounded-0" data-toggle="tab" href="#<?= $row->Id_perusahaan; ?>1">Lowongan Buka</a>
+												</li>
+												<li class="nav-item">
+													<a class="nav-link rounded-0" data-toggle="tab" href="#<?= $row->Id_perusahaan; ?>2">Lowongan Tutup</a>
+												</li>
+											</ul>
 
+											<!-- Tab panes -->
+											<div class="tab-content">
+												<div id="<?= $row->Id_perusahaan; ?>1" class="container tab-pane active">
+													<div class="box-body">
+														<table class="table table-striped">
+															<tbody>
+																	<?php
+																	if ($query->num_rows() > 0) {
+																		$no = 1;
+																		foreach ($query->result() as $jobbuka) {
+																			?>
+																			<?php
+																			$deadline  = date_create($jobbuka->deadline);
+																			$akhir = date_create(); // waktu sekarang
+																			$diff  = date_diff( $deadline, $akhir );
+																			?>
+																			<tr>
+																				<td><?= $no++ ?></td>
+																				<td> <a href="<?= base_url(); ?>company/detailjob_buka/<?= $row->Id_perusahaan ?>/<?= $jobbuka->id_joblist ?>"><?= $jobbuka->Nama_joblist; ?></a></td>
+																				<td>
+																					<span style="color: green;">
+																						<i class="fa fa-clock-o">&nbsp <?= $diff->days; ?> Hari Lagi</i>
+																					</span>
+																				</td>
+																			</tr>
+																		<?php } ?>
+																	</tbody>
+																</table>
+															<?php }
+															else {
+																?>
+																<table class="table table-striped">
+																	<tbody>
+																		<h5 align="center">Kosong</h5>
+																	</tbody>
+																</table>
+															<?php } ?>
+														</div>
+
+													</div>
+													<div id="<?= $row->Id_perusahaan; ?>2" class="container tab-pane fade"><br>
+														<div class="box-body">
+															<table class="table table-striped">
+																<tbody>
+																		<?php
+																		if ($query_tutup->num_rows() > 0) {
+																			$no = 1;
+																			foreach ($query_tutup->result() as $jobtutup) {
+																				?>
+																				<tr>
+																					<td><?= $no++ ?></td>
+																					<td> <a href="<?= base_url(); ?>company/detailjob_tutup/<?= $row->Id_perusahaan ?>/<?= $jobtutup->id_joblist ?>"><?= $jobtutup->Nama_joblist; ?></a></td>
+																				</tr>
+																			<?php } ?>
+																		</tbody>
+																	</table>
+																<?php }
+																else {
+																	?>
+																	<table class="table table-striped">
+																		<tbody>
+																			<h5 align="center">Kosong</h5>
+																		</tbody>
+																	</table>
+																<?php } ?>
+															</div>
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
 							<?php } else { ?>
 
 							<?php } ?>
 
-						<div class="card">
-							<div class="card-header" id="heading-<?= $row->Id_perusahaan ?>">
-								<div class="single-post row">
-									<div class="col-lg-10 col-md-9 profile d-flex align-items-start p-2">
-										<a class="comp-logo col-md-2 pl-0 pr-3" href="<?= base_url(); ?>company/view/<?= $row->Id_perusahaan ?>">
-											<img class="box-shadow p-1" src="<?= base_url() ?>assets/gambar/company/profil/<?= $row->Logo_perusahaan; ?>" width="95px" alt="<?= $row->Nama_perusahaan; ?>">
-										</a>
-										<div>
-											<h4 class="m-0"><a href="<?= base_url(); ?>company/view/<?= $row->Id_perusahaan ?>" class="txt-dark-blue"><strong><?= $row->Nama_perusahaan; ?></strong></a></h4>
-											<span class="d-block txt-dark-blue">
-												<?php foreach ($query2->result() as $prov) ?>
-												<p><?= $prov->nama ?>, Indonesia </p>
-											</span>
-											<p class="mb-1 mt-1"><?= $row->deskripsi_perusahaan; ?></p>
-										</div>
-									</div>
-									<div class="col-lg-2 col-md-3 position text-center d-flex align-items-center p-2">
-										<div class="w-100">
-											<?php
-											$count = 0;
-											foreach ($query->result() as $jobbuka)
-											$count++
-											?>
-										<h1><?= $count ?></h1>
-											<span>Lowongan</span>
-											<h5 class="mb-0">
-												<a data-toggle="collapse" class="<?= $kelas; ?>" href="#collapse-<?= $row->Id_perusahaan ?>" aria-expanded="<?= $ekspand; ?>" aria-controls="collapse-<?= $row->Id_perusahaan ?>">
-												</a>
-											</h5>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div id="collapse-<?= $row->Id_perusahaan ?>" class="<?= $col; ?>" data-parent="#accordion" aria-labelledby="heading-<?= $row->Id_perusahaan ?>">
-								<div class="card-body">
-									<ul class="nav nav-tabs nav-justified" role="tablist">
-										<li class="nav-item">
-											<a class="nav-link active rounded-0" data-toggle="tab" href="#<?= $row->Id_perusahaan; ?>1">Lowongan Buka</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link rounded-0" data-toggle="tab" href="#<?= $row->Id_perusahaan; ?>2">Lowongan Tutup</a>
-										</li>
-									</ul>
 
-									<!-- Tab panes -->
-									<div class="tab-content">
-										<div id="<?= $row->Id_perusahaan; ?>1" class="container tab-pane active">
-											<div class="box-body">
-												<table class="table table-striped">
-													<tbody>
-															<?php
-															if ($query->num_rows() > 0) {
-																$no = 1;
-																foreach ($query->result() as $jobbuka) {
-																	?>
-																	<?php
-																	$deadline  = date_create($jobbuka->deadline);
-																	$akhir = date_create(); // waktu sekarang
-																	$diff  = date_diff( $deadline, $akhir );
-																	?>
-																	<tr>
-																		<td><?= $no++ ?></td>
-																		<td> <a href="<?= base_url(); ?>company/detailjob_buka/<?= $row->Id_perusahaan ?>/<?= $jobbuka->id_joblist ?>"><?= $jobbuka->Nama_joblist; ?></a></td>
-																		<td>
-																			<span style="color: green;">
-																				<i class="fa fa-clock-o">&nbsp <?= $diff->days; ?> Hari Lagi</i>
-																			</span>
-																		</td>
-																	</tr>
-																<?php } ?>
-															</tbody>
-														</table>
-													<?php }
-													else {
-														?>
-														<table class="table table-striped">
-															<tbody>
-																<h5 align="center">Kosong</h5>
-															</tbody>
-														</table>
-													<?php } ?>
-												</div>
-
-											</div>
-											<div id="<?= $row->Id_perusahaan; ?>2" class="container tab-pane fade"><br>
-												<div class="box-body">
-													<table class="table table-striped">
-														<tbody>
-																<?php
-																if ($query_tutup->num_rows() > 0) {
-																	$no = 1;
-																	foreach ($query_tutup->result() as $jobtutup) {
-																		?>
-																		<tr>
-																			<td><?= $no++ ?></td>
-																			<td> <a href="<?= base_url(); ?>company/detailjob_tutup/<?= $row->Id_perusahaan ?>/<?= $jobtutup->id_joblist ?>"><?= $jobtutup->Nama_joblist; ?></a></td>
-																		</tr>
-																	<?php } ?>
-																</tbody>
-															</table>
-														<?php }
-														else {
-															?>
-															<table class="table table-striped">
-																<tbody>
-																	<h5 align="center">Kosong</h5>
-																</tbody>
-															</table>
-														<?php } ?>
-													</div>
-
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
 							<br>
 
 						<?php } ?>
